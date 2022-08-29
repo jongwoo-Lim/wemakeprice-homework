0760e366384bf954a435f95f286eba74f68c6334
limjongwoo <whddn528@naver.com>
2022-08-29 23:34:15 +0900

    chore: java17 -> java11 변경

--- src/main/java/com/wemakeprice/homework/api/model/reader/ItemReaderFactory.java
+++ src/main/java/com/wemakeprice/homework/api/model/reader/ItemReaderFactory.java
@@ -5,10 +5,10 @@ import com.wemakeprice.homework.api.dto.TextRequestDto;
 public class ItemReaderFactory {
 
     public static ItemReader createItemReader(TextRequestDto textRequestDto){
-        return switch (textRequestDto.getType()) {
-            case NO_TAG -> new CrawlingItemReader(textRequestDto.getValue());
-            case TEXT -> new TextItemReader(textRequestDto.getValue());
-            default -> throw new IllegalArgumentException("유효하지 않는 타입입니다.");
-        };
+        switch (textRequestDto.getType()) {
+            case NO_TAG: return  new CrawlingItemReader(textRequestDto.getValue());
+            case TEXT: return new TextItemReader(textRequestDto.getValue());
+            default: throw new IllegalArgumentException("유효하지 않는 타입입니다.");
+        }
     }
 }
