f0d2559069e2be7484fa2bf263dbd18a7c8b40c8
jongwoolim <whddn528@etoos.com>
2022-08-29 15:16:38 +0900

    feat: 몫,나머지 로직 메서드 추가

--- src/main/java/com/wemakeprice/homework/api/model/item/TextItem.java
+++ src/main/java/com/wemakeprice/homework/api/model/item/TextItem.java
@@ -1,8 +1,10 @@
 package com.wemakeprice.homework.api.model.item;
 
+import com.wemakeprice.homework.api.dto.TextResponseDto;
 import com.wemakeprice.homework.api.model.processor.ItemProcessor;
 import com.wemakeprice.homework.api.model.reader.ItemReader;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 @Getter
 public class TextItem {
@@ -29,6 +31,34 @@ public class TextItem {
      */
     public String process(){
         return itemProcessor.process(this.value);
+    }
+
+    /**
+     * 몫, 나머지 구하기
+     * @param text
+     * @param outputSize
+     * @return
+     */
+    public TextResponseDto getQuotientAndRemainder(String text, int outputSize){
+
+        if(StringUtils.hasText(text)){
+            final int textLen = text.length();
+
+            final int quotient = textLen / outputSize;
+
+            final String strQuotient = text.substring(0, (outputSize * quotient));
+            final String strRemainder = text.substring(outputSize * quotient);
+
+            return TextResponseDto.builder()
+                    .quotient(strQuotient)
+                    .remainder(strRemainder)
+                    .build();
+        }
+
+        return TextResponseDto.builder()
+                .quotient("")
+                .remainder("")
+                .build();
     }
 
 }
